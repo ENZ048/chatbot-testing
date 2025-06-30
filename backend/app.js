3d578d4a80895db6c7fc8813a254a637284f8ce1
Pratik <pratik.yesare68@gmail.com>
2025-06-30 09:43:50 +0530

    Fixed url

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(cors({
     "https://chatbot-testing-7m25.vercel.app",              // chatbot widget
     "https://chatbot-testing-wqxk.onrender.com",               // client websites (optional)
     "https://chatbot-demo-livid.vercel.app",  // demo chatbot
-    "https://troikatech.ai/pratik" //test
+    "https://troikatech.ai" //test
   ],
   credentials: true
 }));
